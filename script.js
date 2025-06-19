8e1379130c0b5c70f7be94f592e61ca91f92a300
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-06-19 23:45:37 +0200

    fixed previous logic - add logic to add "x" or "o" into cells

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 /* 
--impostare una condizione che se l’utente clicca sul bottone player 1, l’unico carattere che si può inserire nei quadrati è X; dopo che ha cliccato, l’unico carattere che si può inserire è O.
+-Impostare una condizione che se l’utente clicca sul bottone player 1, l’unico carattere che si può inserire nei quadrati è X; dopo che ha cliccato, l’unico carattere che si può inserire è O.
 
-Impostare la variabile:
+1-Impostare la variabile:
 let currentPlayer = “”;
 
-Impostare una funzione che cliccando su player 1: 
+2-Impostare una funzione che cliccando su player 1: 
 let currentPlayer = “x”;
 
-Impostare una funzione che cliccando su player 2:
+3-Impostare una funzione che cliccando su player 2:
 let currentPlayer = “o”;
 
-Impostare una funzione che cliccando su un quadrato:
+4-Impostare una funzione che cliccando su una cella:
 Se currentPlayer = “X”; 
 Viene inserito il testo X
 Poi impostato currentPlayer = “O”;
@@ -22,34 +22,60 @@ let currentPlayer = “X”;
 //variable for understand how is current player 
 let currentPlayer = "";
 
-//elements from dom
+//get elements from dom
 //player bottuns 
 let btnPlayer1 = document.getElementById("btn-player1");
 let btnPlayer2 = document.getElementById("btn-player2");
 //cells
-let cell1 = document.getElementById("cell-1");
-let cell2 = document.getElementById("cell-2");
-let cell3 = document.getElementById("cell-3");
-let cell4 = document.getElementById("cell-4");
-let cell5 = document.getElementById("cell-5");
-let cell6 = document.getElementById("cell-6");
-let cell7 = document.getElementById("cell-7");
-let cell8 = document.getElementById("cell-8");
-let cell9 = document.getElementById("cell-9");
+let cells = document.querySelectorAll(".cell");
 
 //functions
 //current player
 function currentPlayer1(){
     currentPlayer = "x";
-    console.log("current player: " + currentPlayer);
-    
+    console.log("Start Player1 (" + currentPlayer + ")");  
 }
 
 function currentPlayer2(){
     currentPlayer = "o";
-    console.log("current player: " + currentPlayer);
+    console.log("Start Player2 (" + currentPlayer + ")");
 }
 
-btnPlayer1.onclick = currentPlayer1;
-btnPlayer2.onclick = currentPlayer2;
+//add "x" or "o" into cell
+function cellElement(){
+    cells.forEach(cell => {
+        cell.onclick = function(event) {
+            let cell = event.target;
+            console.log(cell);
+            console.log('The clicked cell has id: ' + cell.id);
+            if(cell.textContent === ""){
+                if(currentPlayer === "x"){
+                    cell.textContent = "x";
+                    currentPlayer = "o";
+                }else{
+                    cell.textContent = "o";
+                    currentPlayer = "x";
+                }
+            }else{
+                console.log("The cell is already occupied");
+            }
+        };
+    });
+}
+
+//test
+btnPlayer1.onclick = function(){
+    currentPlayer1();
+    cellElement();
+}
+
+btnPlayer2.onclick = function(){
+    currentPlayer1();
+    cellElement();
+}
+
+
+
+
+
 
